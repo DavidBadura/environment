902bec5971d093b5872ea93126204c6c93d4413a
DavidBadura <d.badura@gmx.de>
2013-12-17 11:31:34 +0100

    add composer & php-cs-fixer scripts

---
+++ scripts/composer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
---
+++ scripts/php-cs-fixer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo curl http://cs.sensiolabs.org/get/php-cs-fixer.phar -o /usr/local/bin/php-cs-fixer
+sudo chmod a+x /usr/local/bin/php-cs-fixer
