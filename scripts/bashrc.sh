9cf822f5703ec04027f415c7968b7d44de66cc5b
DavidBadura <d.badura@gmx.de>
2014-01-02 11:51:00 +0100

    update bashrc

--- scripts/bashrc.sh
+++ scripts/bashrc.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-curl -o ~/.git-prompt.sh https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
+if ! [ "`type -t __git_ps1`" == 'function' ]
+    then
+        curl -o ~/.git-prompt.sh https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
+fi
+
 echo 'source ~/environment/files/bashrc' >> ~/.bashrc
