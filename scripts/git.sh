c814442279c02df5768c3e05856ed3c00ac930f7
David Badura <d.badura@gmx.de>
2015-11-25 10:55:21 +0100

    update git config

--- scripts/git.sh
+++ scripts/git.sh
@@ -6,13 +6,16 @@ git config --global color.ui auto
 git config --global core.editor vim
 git config --global core.excludesfile '~/environment/files/gitignore'
 git config --global push.default "current"
-git config --global help.autocorrect 1
+git config --global help.autocorrect 3000
 git config --global branch.autosetuprebase always
 
 git config --global alias.st status
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.br branch
+
+git config --global alias.pull pull --rebase
+
 git config --global alias.staged 'diff --cached'
 git config --global alias.graph "log --graph --all --pretty=format:'%Cred%h%Creset - %Cgreen(%cr)%Creset %s%C(yellow)%d%Creset' --abbrev-commit --date=relative"
 git config --global alias.changes "!git log --name-status HEAD.."
