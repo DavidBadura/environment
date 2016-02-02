145e6c4d3050bac53bccc1ed8816b64e4a6946e3
David Badura <d.badura@gmx.de>
2016-02-02 11:30:21 +0100

    update git config

--- scripts/git.sh
+++ scripts/git.sh
@@ -14,7 +14,12 @@ git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.br branch
 
-git config --global alias.pull pull --rebase
+git config --global alias.up "push -u"
+git config --global alias.pu "pull --rebase"
+git config --global alias.undo "reset HEAD~1 --mixed"
+git config --global alias.amend "commit -a --amend"
+git config --global alias.aa "add -A"
+git config --global alias.fu "reset --hard"
 
 git config --global alias.staged 'diff --cached'
 git config --global alias.graph "log --graph --all --pretty=format:'%Cred%h%Creset - %Cgreen(%cr)%Creset %s%C(yellow)%d%Creset' --abbrev-commit --date=relative"
